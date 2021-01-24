274f67cd2150d7f2d7725d2a1c9655f3f08aa9d2
Miguel Angel <angelarkangel91@hotmail.com>
2021-01-24 15:42:34 -0600

    Updates

---
+++ precache-manifest.4b60082bf63cebc8692337ca43da2879.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "456c86c8b09b98facb82a6f4d02b86ab",
+    "url": "/sort/index.html"
+  },
+  {
+    "revision": "05fbe4a22a63e77f0044",
+    "url": "/sort/static/css/main.25daafd3.chunk.css"
+  },
+  {
+    "revision": "aa9d510a67db5b94ed2b",
+    "url": "/sort/static/js/2.dd06e1ab.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/sort/static/js/2.dd06e1ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05fbe4a22a63e77f0044",
+    "url": "/sort/static/js/main.19c36470.chunk.js"
+  },
+  {
+    "revision": "0ac57fb41b414597e87f",
+    "url": "/sort/static/js/runtime-main.572d3811.js"
+  }
+]);
